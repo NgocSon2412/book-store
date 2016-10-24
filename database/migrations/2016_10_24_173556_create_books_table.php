20f6e88cfae13639cc69d44e70ddf6455e2e8fe3
NgocSon2412 <sonnguyenk57@gmail.com>
2016-10-25 00:47:18 +0700

    init model

---
+++ database/migrations/2016_10_24_173556_create_books_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->integer('category_id');
+            $table->integer('author_id');
+            $table->integer('price');
+            $table->string('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('books');
+    }
+}
